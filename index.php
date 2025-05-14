adf50cd358fab76a2e6139094cda877104f0d1d6
TatianaPeranquive <tatianaperanquive@gmail.com>
2025-05-14 17:52:17 -0500

    send data from GET method (AP)

---
+++ index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <form action="procesar.php" method="GET">
+        <label for="name"> NAME: </label>
+        <input id= "name" name="name" type="text">
+        <button type="submit"> Enviar </button>
+    </form>
+</body>
+</html>
---
+++ procesar.php
@@ -0,0 +1,6 @@
+
+<?php 
+$nombre = $_GET['name'];
+//var_dump($_GET);
+echo "<p> El nombre del usuario es: $nombre </p>";
+
