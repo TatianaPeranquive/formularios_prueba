2200cd3dda09eb1a981d680565ee003e8c7b3dfe
TatianaPeranquive <tatianaperanquive@gmail.com>
2025-05-16 15:47:58 -0500

    One interface can extends from several inerfaces and the class that used it, should implements all methods from all interfaces

--- index.php
+++ index.php
@@ -3,15 +3,19 @@ interface iTemplate{
     public function a();    
 }
 
-interface iTemplateExtend extends iTemplate{
+interface iTemplateExtend {
     public function b();
 }
 
-//Class a contains 1 abstract method and must therefore be declared abstract or implement the remaining methods (iTemplate::a)
-class a implements iTemplateExtend{
- public function b(){}
- public function a(){}
+interface iTemplateExtend2 extends iTemplate, iTemplateExtend{
+    public function c();
+}
 
-public function setVariable(object $user){}
+//Fatal error: Class implementInterfaces contains 1 abstract method 
+//and must therefore be declared abstract or implement the remaining methods (iTemplateExtend2::c)
+class implementInterfaces implements iTemplateExtend2{
+  public function a(){}
+  public function b(){}
+  public function c(){}
 
 }
