728181d4ff6502a444b8defc51b42d0cb81b3a9c
TatianaPeranquive <tatianaperanquive@gmail.com>
2025-05-17 20:19:45 -0500

    Declare a propertie like "static" allow us to access to this property without having to instantiate an object

--- clases/statics/index.php
+++ clases/statics/index.php
@@ -1,10 +1,13 @@
 <?php 
 
 class Humano{
+
+    public static $nombre = "Anggy PG";
     public static function saludar(){
         echo "Holis";
     }
 }
 
 Humano::saludar();
+echo Humano::$nombre;
 ?>
