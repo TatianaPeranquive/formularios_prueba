4c9294f31cb584f9b2cc5d5507326d83ae7ce37b
TatianaPeranquive <tatianaperanquive@gmail.com>
2025-05-17 19:45:11 -0500

    If there must be multiple autoload functions, spl_autoload_register() allows for this.

--- clases/namespace_top/index.php
+++ clases/namespace_top/index.php
@@ -1,13 +1,19 @@
 <?php
 
 use Carpeta1\Humano; 
-use Carpeta2\Humano as Humano2 ; // Forma2 para usar varios namespace 
-require_once('Carpeta1\Humano.php');
-require_once('Carpeta2\Humano.php');
+use Carpeta2\Humano as Humano2 ; 
+
+ //echo str_replace('\\' , '/', $clase) . '.php'; // Carpeta1\Humano; sea remplazado por -->  Carpeta1\Humano
+spl_autoload_register(function($clase){
+require_once(str_replace('\\' , '/', $clase). '.php' );
+});
+
+//require_once('Carpeta1\Humano.php');
+//require_once('Carpeta2\Humano.php');
 
 
-$humano1 = new Humano;
+$humano1 = new Carpeta1\Humano;
 $humano1->saludar();
 
-$humano2 = new Carpeta2\Humano;
+$humano2 = new Humano2;
 $humano2->saludar();
