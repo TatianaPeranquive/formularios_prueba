ffa93ccb2d5c68fd207c8d980c5724531faa70bf
TatianaPeranquive <tatianaperanquive@gmail.com>
2025-05-17 20:36:05 -0500

    As $nombre is a static property can  be called only within the own class with the keyword self::property

--- clases/statics/index.php
+++ clases/statics/index.php
@@ -2,12 +2,18 @@
 
 class Humano{
 
-    public static $nombre = "Anggy PG";
+    public static $nombre = "Anggy PG"; // ---> le pertenece unicamente a esta clase, no se le pasa al objeto 
     public static function saludar(){
         echo "Holis";
     }
+    public function saludoPersonalizado(){
+        echo "Señorita " . self::$nombre; // --> this hace referencia a la propiedad del objeeto instanciado
+                                                // Si uso $this->nombre sale el error:
+                                                //Notice: Accessing static property Humano::$nombre as non static
+                                                //Warning: Undefined property: Humano::$nombre 
+    }
 }
+$humano = new Humano;
+$humano->saludoPersonalizado();
 
-Humano::saludar();
-echo Humano::$nombre;
 ?>
