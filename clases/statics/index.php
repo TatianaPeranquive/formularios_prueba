4b913c7a7e921be40efcf997f13a1da8af0b2e97
TatianaPeranquive <tatianaperanquive@gmail.com>
2025-05-17 20:41:40 -0500

    parent::$nombre is used by the child class to access a parent's static property

--- clases/statics/index.php
+++ clases/statics/index.php
@@ -2,18 +2,25 @@
 
 class Humano{
 
-    public static $nombre = "Anggy PG"; // ---> le pertenece unicamente a esta clase, no se le pasa al objeto 
+public static $nombre = "Anggy PG";     // ---> le pertenece unicamente a esta clase, no se le pasa al objeto 
     public static function saludar(){
         echo "Holis";
     }
     public function saludoPersonalizado(){
-        echo "Señorita " . self::$nombre; // --> this hace referencia a la propiedad del objeeto instanciado
+        echo "Señorita " . self::$nombre;   // --> this hace referencia a la propiedad del objeeto instanciado
                                                 // Si uso $this->nombre sale el error:
                                                 //Notice: Accessing static property Humano::$nombre as non static
                                                 //Warning: Undefined property: Humano::$nombre 
     }
 }
-$humano = new Humano;
-$humano->saludoPersonalizado();
 
+class Colombiano extends Humano{
+    function saludoColombiano() {
+        echo "Sumercé como está  ". parent::$nombre;  // --> Como $nombre es propiedad estática del padre, solo se puede
+                                                    // instanciar con la palabra clave parent::$nombre
+    }
+}
+
+$Colombiano = new Colombiano;
+$Colombiano->saludoColombiano();
 ?>
